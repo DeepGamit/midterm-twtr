d1d4345d45c4972251a2dcb61c793916050d10fd
DeepGamit <deepkgamit@gmail.com>
2021-11-21 00:59:52 -0500

    MongoDB DB IP Change

--- be/twtr.py
+++ be/twtr.py
@@ -31,7 +31,7 @@ import jwt
 g = dict()
 
 # mongo
-mongo_client = MongoClient('mongodb://10.0.2.30:27017/')
+mongo_client = MongoClient('mongodb://3.228.80.110:27017/')
 # mongo_client = MongoClient("mongodb+srv://admin:admin@tweets.8ugzv.mongodb.net/tweets?retryWrites=true&w=majority")
 
 app = Flask(__name__)
